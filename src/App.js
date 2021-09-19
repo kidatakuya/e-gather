4b3195dfdb4d09b94e6bf5285a820b400602223c
kidatakuya <kida.takuya.1234@gmail.com>
2021-09-19 10:29:34 +0900

    Solving the white problem at build time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import {HashRouter as Router,Switch,Route,Link} from "react-router-dom";
 import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
 
